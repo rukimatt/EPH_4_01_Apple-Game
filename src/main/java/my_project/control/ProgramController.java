734837e08a5cbea362b9d5743f4435bac5591438
Mattea <rukimatt@kkg-do.eu>
2023-04-04 17:07:34 +0200

    Zuhause

--- src/main/java/my_project/control/ProgramController.java
+++ src/main/java/my_project/control/ProgramController.java
@@ -18,8 +18,8 @@ public class ProgramController {
     // Referenzen
     private ViewController viewController;  // diese Referenz soll auf ein Objekt der Klasse viewController zeigen. Über dieses Objekt wird das Fenster gesteuert.
 
-    private Apple apple01;
-    private Pear pear01;
+    private Apple apple01, apple02, apple03, apple04, apple05;
+    private Pear pear01, pear02, pear03, pear04, pear05;
     private Player player01;
 
     /**
@@ -46,8 +46,48 @@ public class ProgramController {
 
         xPos = Math.random() * (Config.WINDOW_WIDTH - 50) + 50;
         yPos = Math.random() * (Config.WINDOW_HEIGHT - 50) + 50;
+        apple02 = new Apple(xPos, yPos);
+        viewController.draw(apple02);
+
+        xPos = Math.random() * (Config.WINDOW_WIDTH - 50) + 50;
+        yPos = Math.random() * (Config.WINDOW_HEIGHT - 50) + 50;
+        apple03 = new Apple(xPos, yPos);
+        viewController.draw(apple03);
+
+        xPos = Math.random() * (Config.WINDOW_WIDTH - 50) + 50;
+        yPos = Math.random() * (Config.WINDOW_HEIGHT - 50) + 50;
+        apple04 = new Apple(xPos, yPos);
+        viewController.draw(apple04);
+
+        xPos = Math.random() * (Config.WINDOW_WIDTH - 50) + 50;
+        yPos = Math.random() * (Config.WINDOW_HEIGHT - 50) + 50;
+        apple05 = new Apple(xPos, yPos);
+        viewController.draw(apple05);
+
+        xPos = Math.random() * (Config.WINDOW_WIDTH - 50) + 50;
+        yPos = Math.random() * (Config.WINDOW_HEIGHT - 50) + 50;
         pear01 = new Pear(xPos, yPos);
         viewController.draw(pear01);
+
+        xPos = Math.random() * (Config.WINDOW_WIDTH - 50) + 50;
+        yPos = Math.random() * (Config.WINDOW_HEIGHT - 50) + 50;
+        pear02 = new Pear(xPos, yPos);
+        viewController.draw(pear02);
+
+        xPos = Math.random() * (Config.WINDOW_WIDTH - 50) + 50;
+        yPos = Math.random() * (Config.WINDOW_HEIGHT - 50) + 50;
+        pear03 = new Pear(xPos, yPos);
+        viewController.draw(pear03);
+
+        xPos = Math.random() * (Config.WINDOW_WIDTH - 50) + 50;
+        yPos = Math.random() * (Config.WINDOW_HEIGHT - 50) + 50;
+        pear04 = new Pear(xPos, yPos);
+        viewController.draw(pear04);
+
+        xPos = Math.random() * (Config.WINDOW_WIDTH - 50) + 50;
+        yPos = Math.random() * (Config.WINDOW_HEIGHT - 50) + 50;
+        pear05 = new Pear(xPos, yPos);
+        viewController.draw(pear05);
 
         player01 = new Player(50, Config.WINDOW_HEIGHT - 100);
         viewController.draw(player01);
@@ -64,7 +104,40 @@ public class ProgramController {
         if (checkAndHandleCollision(pear01)) {
             pear01.jumpBack();
         }
+
+        if (checkAndHandleCollision(pear02)) {
+            pear01.jumpBack();
+        }
+
+        if (checkAndHandleCollision(pear03)) {
+            pear01.jumpBack();
+        }
+
+        if (checkAndHandleCollision(pear04)) {
+            pear01.jumpBack();
+        }
+
+        if (checkAndHandleCollision(pear05)) {
+            pear01.jumpBack();
+        }
+
         if (checkAndHandleCollision(apple01)) {
+            apple01.jumpBack();
+        }
+
+        if (checkAndHandleCollision(apple02)) {
+            apple01.jumpBack();
+        }
+
+        if (checkAndHandleCollision(apple03)) {
+            apple01.jumpBack();
+        }
+
+        if (checkAndHandleCollision(apple04)) {
+            apple01.jumpBack();
+        }
+
+        if (checkAndHandleCollision(apple05)) {
             apple01.jumpBack();
         }
         //Weitere TODOs folgen und werden im Unterricht formuliert. Spätestens nach TODO 08 sollte der Aufbau des Projekts durchdacht werden.
